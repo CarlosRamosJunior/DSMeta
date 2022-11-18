00e3c7b8a5b9ce8382c02c8ee98216365f521a54
Cr3Junior <cr3programacao@gmail.com>
2022-11-17 21:58:42 -0300

    Header

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,9 +1,11 @@
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+
 
 function App() {
   return(
    <> 
-     <h1>Ola mundo</h1>
+    <Header />
      <NotificationButton />
      <NotificationButton />
      <NotificationButton />
---
+++ FrontEnd/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from "../../assets/img/logo.svg"
+
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-conteiner">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por mim 
+                    <a href="https://www.instagram.com/cr3junior/">@cr3junior</a>
+                </p>
+            </div>
+
+        </header>
+    )
+}
+
+export default Header
